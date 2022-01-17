7fc8654b78012cf3fb2037706685e29a5a45c2a6
Harpreetkaur27 <94538609+Harpreetkaur27@users.noreply.github.com>
2022-01-17 17:49:14 +0530

    Update shape.js

--- shape.js
+++ shape.js
@@ -1,1 +1,29 @@
+// import using require
+const Shape = require('./shape');
+// declare class
 
+class Circle extends Shape {
+	constructor() {
+		super();
+	}  
+	calculateArea() {}
+}
+
+// export class using module.exports
+module.exports = Circle;
+
+
+// declare class
+
+class Shape {
+  constructor() {
+    this.color = "red';
+  }
+  calculateArea() {}
+  drawShape() {}
+}
+
+// Shape.color = '...' //! Static properties
+
+module.exports = Shape;
+// export class using module.exports

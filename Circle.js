4cff484220cfb2c8eaa0bc4af3ff627348cc4d4c
Harpreetkaur27 <94538609+Harpreetkaur27@users.noreply.github.com>
2022-01-17 18:07:06 +0530

    Create Circle.js

---
+++ Circle.js
@@ -0,0 +1,13 @@
+// import using require
+const Shape = require('./Shape');
+// declare class
+
+class Circle extends Shape {
+	constructor() {
+		super();
+	}  
+	calculateArea() {}
+}
+
+// export class using module.exports
+module.exports = Circle;
